c549c511ba7a9ef63c98cb6fa1ff8b27d5970388
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-04-20 00:46:24 +0000

    WIP No. 2 - Fixed animation layout.

--- src/main.py
+++ src/main.py
@@ -63,25 +63,68 @@ class MainOrganization(Slide):
             "assets/centerstage_tree.txt", 
             tab_width=4
         ).next_to(title, DOWN)
+        note = Text("Further descriptions are available in the repository release section.")
 
         title.set_z_index(100)
+        note.scale(0.5).to_edge(DOWN)
 
         self.play(FadeIn(title), FadeIn(tree))
-        self.play(tree.animate.scale(0.5).next_to(title, DOWN))
+        self.play(tree.animate.scale(0.425).next_to(title, DOWN), Write(note))
 
         self.next_slide()
 
-        self.play(tree.animate.scale(1.4).next_to(title, DOWN).to_edge(LEFT), tree.background_mobject.animate.set_opacity(0))
+        self.play(tree.animate.scale(1.4).next_to(title, DOWN).to_edge(LEFT), tree.background_mobject.animate.set_opacity(0), FadeOut(note))
         set_code_fade(tree, 2.8)
 
         data = [
-            (17, "alpha, beta, and gamma are old codebases.\nYou can ignore them.")
+            (
+                17, 
+                "alpha, beta, and gamma are old codebases.\nYou can ignore them.", 
+                lambda : [], 
+                lambda : []
+            ), 
+            (
+                20, 
+                "delta is the most up-to-date version.\nBe sure to reference the dev2 branch instead of the main branch\n(see the note in README on the main branch).", 
+                lambda : [], 
+                lambda : []
+            ), 
+            (12, 
+                "mollusc library.", 
+                lambda : [], 
+                lambda : []
+            ), 
+            (
+                29, 
+                """Autonomous
+- Initialization and drive functionality through mollusc.
+- Hardware set up through SquidWare.
+- Uses TotemPipeline as the image processing pipeline for detecting the team prop.
+- May use various subsystems to score.""", 
+                lambda : [
+                    Transform(title, Title("Organization | Autonomous")), 
+                    highlight_line(tree, 22), 
+                    highlight_line(tree, 23), 
+                    highlight_line(tree, 31)
+                ], 
+                lambda : [
+                    unhighlight_line(tree, 22), 
+                    unhighlight_line(tree, 23), 
+                    unhighlight_line(tree, 31)
+                ]
+            )
         ]
 
-        for line_num, text in data:
-            focus_line(self, tree, line_num, 0)
-            dialog = create_code_dialog(self, tree, line_num, 1.5, 0.1, 5, text)
+        for line_num, text, extra_anims, unload_extra_anims in data:
+            focus_line(self, tree, line_num, 0, add_to_buffer=True)
+            play_animation_buffer(self)
+
+            dialog = create_code_dialog(self, tree, line_num, -1, 0.1, 7, text, add_to_buffer=True)
+            animation_buffer.extend(extra_anims())
+            play_animation_buffer(self)
 
             self.next_slide()
 
-            remove_code_dialog(self, dialog)
+            remove_code_dialog(self, dialog, add_to_buffer=True)
+            animation_buffer.extend(unload_extra_anims())
+        play_animation_buffer(self)
--- src/util.py
+++ src/util.py
@@ -1,6 +1,13 @@
 from manim import *
 from manim_slides import Slide
 
+animation_buffer = []
+
+def play_animation_buffer(scene: Slide):
+    global animation_buffer
+    scene.play(*animation_buffer)
+    animation_buffer.clear()
+
 def fade_out_all(scene: Slide):
     scene.play(*[FadeOut(i) for i in scene.mobjects])
 
@@ -17,25 +24,55 @@ def set_code_fade(code: Code, max_y: float):
     for line in code.code.chars:
         line.add_updater(updater)
 
-def focus_line(scene: Slide, code: Code, line: int, target_y: float):
+def focus_line(scene: Slide, code: Code, line: int, target_y: float, add_to_buffer=False):
+    global animation_buffer
     vgroup = code.line_numbers.chars[line - 1]
     dy = target_y - vgroup.get_y()
-    scene.play(code.animate.shift(dy * UP))
+    anim = code.animate.shift(dy * UP)
+    if add_to_buffer:
+        animation_buffer.append(anim)
+    else:
+        scene.play(anim)
 
-def create_code_dialog(scene: Slide, code: Code, line: int, indicator_length: float, padding: float, text_width: float, text: str):
-    vgroup = code.code.chars[line - 1]    
-    indicator = Line(start=[0, 0, 0], end=[indicator_length, 0, 0])
+def create_code_dialog(scene: Slide, code: Code, line: int, end_position: float, padding: float, text_width: float, text: str, add_to_buffer=False):
+    global animation_buffer
+    vgroup = code.code.chars[line - 1]
+    x = vgroup.get_x()
+    y = vgroup.get_y()
+    indicator = Line(start=[x + vgroup.width / 2, y, 0], end=[end_position, y, 0])
+    # indicator = Line([0, 0, 0], end=[end_position, 0, 0])
     description = Paragraph(text)
-    border = Rectangle(width=text_width + 2 * padding, height=description.height + 2 * padding)
-
     description.width = text_width
-    indicator.next_to(vgroup, RIGHT)
+    border = SurroundingRectangle(description, buff=padding, color=WHITE)
+
+    # indicator.next_to(vgroup, RIGHT)
     description.next_to(indicator, RIGHT)
-    description.shift(padding * RIGHT)
     border.move_to(description)
 
-    scene.play(Create(indicator), Write(description), Create(border))
+    anims = (Create(indicator), Write(description), Create(border))
+    if add_to_buffer:
+        animation_buffer.extend(anims)
+    else:
+        scene.play(*anims)
     return indicator, description, border
 
-def remove_code_dialog(scene: Slide, dialog):
-    scene.play(Uncreate(dialog[0]), Unwrite(dialog[1]), Uncreate(dialog[2]))
+def remove_code_dialog(scene: Slide, dialog, add_to_buffer=False):
+    global animation_buffer
+
+    anims = (Uncreate(dialog[0]), FadeOut(dialog[1]), Uncreate(dialog[2]))
+    if add_to_buffer:
+        animation_buffer.extend(anims)
+    else:
+        scene.play(*anims)
+
+hl_surrounding = dict()
+
+def highlight_line(code: Code, line_num: int):
+    global hl_surrounding
+    r = SurroundingRectangle(code.code.chars[line_num - 1], color=PURE_GREEN, buff=0)
+    hl_surrounding[line_num] = r
+    return Create(r)
+
+def unhighlight_line(code: Code, line_num: int):
+    global hl_surrounding
+    return Uncreate(hl_surrounding.pop(line_num))
