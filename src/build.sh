55d1c62170f368368622d837beec008207c9d621
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-11-02 12:54:55 +0000

    No comment.

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SCENES=$(cat src/scenes.txt)
-echo "src/main.py ${SCENES}" | xargs manim
+echo "-ql src/main.py ${SCENES}" | xargs manim
 echo "convert ${SCENES} _site/index.html" | xargs manim-slides
 
 if [ ! -z $1 ] && [ $1 = "final" ]; then
